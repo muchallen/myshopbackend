49aeee57dcc3616ba37321748068d1fbe8744eb8
muchallen <muchallen@gmail.com>
2021-07-10 13:58:03 +0200

    api

--- src/main/java/com/muchallen/shop/ShopApplication.java
+++ src/main/java/com/muchallen/shop/ShopApplication.java
@@ -52,8 +52,8 @@ public class ShopApplication {
 		return  new WebMvcConfigurer() {
 			@Override
 			public void addResourceHandlers(ResourceHandlerRegistry registry) {
-				registry.addResourceHandler("/app/target/shop-0.0.1-SNAPSHOT.jar!/BOOT-INF/classes!/static/successImage/content")
-						.addResourceLocations("file:/app/target/shop-0.0.1-SNAPSHOT.jar!/BOOT-INF/classes!/static/successImage/content");
+				registry.addResourceHandler("/app/target/shop-0.0.1-SNAPSHOT.jar!/BOOT-INF/classes!/static/successImage/")
+						.addResourceLocations("file:/app/target/shop-0.0.1-SNAPSHOT.jar!/BOOT-INF/classes!/static/successImage/");
 			}
 		};
 	}
